4f8a0c03e541b7a8af16fb6ad8b2c47edef646fe
JavDel88 <tm881126@gmail.com>
2022-04-05 08:46:31 -0500

    add loading and styles

---
+++ src/components/Loading.js
@@ -0,0 +1,20 @@
+import "./Loading.css";
+
+const Loading = () => {
+  return (
+    <div className="loadingContainer">
+      <div class="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
--- src/services/getWeather.js
+++ src/services/getWeather.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const getWeather = (lat, lon) => {
-    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7db02ee52aeb100aae7598facd0b6951&units=metric&lang=es`
-    // const URL = `http://api.weatherapi.com/v1/current.json?key=e7b1be4a0cd8440188d220030220104&q=${lat},${lon}`
-    const res = axios.get(URL)
+const getWeather = async (lat, lon) => {
+    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7db02ee52aeb100aae7598facd0b6951&units=metric&lang=es`   
+    const res = await axios.get(URL)
     return res
 }
 
